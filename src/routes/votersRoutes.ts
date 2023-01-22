d00367e8b27b00999da33d9218a642a09961ed8d
Emmanuel Oreoluwa <awuloero13@gmail.com>
2023-01-22 14:00:00 +0100

    update

--- src/routes/votersRoutes.ts
+++ src/routes/votersRoutes.ts
@@ -4,7 +4,7 @@ import { IVoter } from "../types/types";
 const router: Router = Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello");
 });
 
 export default router;
