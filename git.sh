00b17af71ac34ce93dd66c803bb6498159375048
Emmanuel Oreoluwa <awuloero13@gmail.com>
2023-03-19 09:13:52 +0100

    vscode autosave

--- git.sh
+++ git.sh
@@ -7,9 +7,9 @@ s=("src/controllers/voteController.ts" "src/routes/voteRoutes.ts" "package-lock.
 
 for i in "${s[@]}"
 do
-    echo git add $i
-    read -p "Enter commit message: " commitMessage
-    echo "git commit -m $commitMessage"
-    echo "git push"
-    git add $i
+     git add $i
+     read -p "Enter commit message: " commitMessage
+     git commit -m $commitMessage"
+     git push"
+     git add $i
 done
