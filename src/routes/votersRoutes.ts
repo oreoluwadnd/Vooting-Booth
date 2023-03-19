132ea7f0be04b3fe90c9e8f164ec7e5a9a05fd55
Emmanuel Oreoluwa <awuloero13@gmail.com>
2023-03-19 09:10:28 +0100

    vscode autosave

--- src/routes/votersRoutes.ts
+++ src/routes/votersRoutes.ts
@@ -16,4 +16,9 @@ const router: Router = Router();
 router.post("/register", setAdmin, checkEmail, register);
 router.post("/login", login);
 
+router.route("/getAllVoters").get(getAllVoters);
+router.route("/getVoter/:id").get(getVoter);
+router.route("/deleteVoter/:id").delete(deleteVoter);
+router.route("/updateVoter/:id").patch(updateVoter);
+
 export default router;
